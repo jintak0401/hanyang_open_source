8782431842374c72ad7e6c84fecfdbb33f7b693e
HashDigest <92960296+HashDigest@users.noreply.github.com>
2021-10-25 10:07:33 +0900

    원하는 커밋 메시지 ( 예시: add 여러분 ID data )

--- data/index.js
+++ data/index.js
@@ -1,5 +1,5 @@
-import jintak0401 from './jintak0401';
-import HashDigest from './HashDigest';
+import jintak0401 from "./jintak0401";
+import HashDigest from "./HashDigest";
 
 export {
     jintak0401,
