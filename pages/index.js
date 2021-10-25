06a48d4fb8fd73bd249fb36fd4e028a034ff397b
jintak0401 <jintak0401@naver.com>
2021-10-25 21:39:49 +0900

    fix meta tag

--- pages/index.js
+++ pages/index.js
@@ -36,7 +36,7 @@ export default function Home() {
                 <meta name="description" content="한양대생들이 오픈소스에 기여하는 기회를 제공하기 위해 제작하였습니다."/>
                 <meta property="og:title" content="한양오픈소스 프로젝트"/>
                 <meta property="og:description" content="쓰고 싶은 아무 글이나 써서 오픈소스에 기여하세요. 한양대생들이 오픈소스에 친숙해질 수 있도록 제작했습니다."/>
-                <meta property="og:image" content="https://hanyang-open-source.vercel.app/images/meta.png"/>
+                <meta property="og:image" content="https://hanyang-open-source.vercel.app/meta.png"/>
                 <meta name="twitter:description" content="한양대생들이 오픈소스에 기여하는 기회를 제공하기 위해 제작하였습니다."/>
                 <meta name="twitter:title" content="한양오픈소스 프로젝트"/>
                 <meta name="viewport" content="width=device-width"/>
