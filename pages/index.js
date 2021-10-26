4d6fc7bfc4fc6720045ac12a7e9235bdbe0895b9
jintak0401 <jintak0401@naver.com>
2021-10-26 21:39:48 +0900

    feat: add underline on example and change meta.png

--- pages/index.js
+++ pages/index.js
@@ -48,8 +48,6 @@ export default function Home() {
                 <meta name="twitter:description" content="한양대생들이 오픈소스에 기여하는 기회를 제공하기 위해 제작하였습니다."/>
                 <meta name="twitter:title" content="한양오픈소스 프로젝트"/>
                 <meta property="og:locale" content="ko_KR"/>
-                {/*<meta property="og:image:width" content="1200"/>*/}
-                {/*<meta property="og:image:height" content="630"/>*/}
                 <link rel="icon" href="/hanyangLion.ico"/>
             </Head>
 
