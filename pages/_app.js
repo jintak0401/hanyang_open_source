83a4c0dd1bd7073cf39c7bea5f26c97d76d68df8
jintak0401 <jintak0401@naver.com>
2021-10-27 10:53:04 +0900

    fix: change .env

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
-import {useEffect} from 'react'
-import {useRouter} from 'next/router'
-import * as gtag from '../lib/gtag'
-import '../styles/globals.css'
+import {useEffect} from 'react';
+import {useRouter} from 'next/router';
+import * as gtag from '@lib/gtag';
+import '../styles/globals.css';
 
 const App = ({Component, pageProps}) => {
     const router = useRouter();
--- pages/_document.js
+++ pages/_document.js
@@ -1,5 +1,4 @@
 import Document, {Html, Head, Main, NextScript} from 'next/document';
-import {GA_TRACKING_ID} from '../lib/gtag';
 
 export default class MyDocument extends Document {
     render() {
@@ -9,7 +8,7 @@ export default class MyDocument extends Document {
                     {/* Global Site Tag (gtag.js) - Google Analytics */}
                     <script
                         async
-                        src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+                        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
                     />
                     <script dangerouslySetInnerHTML={{
                         __html: `
@@ -17,7 +16,7 @@ export default class MyDocument extends Document {
                             function gtag(){dataLayer.push(arguments);}
                             gtag('js', new Date());
 
-                            gtag('config', '${GA_TRACKING_ID}', {
+                            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
                               page_path: window.location.pathname,
                             });
                       `,
